4e17a8e6266447b49d03268f3d71a645902ffbe3
Mark Delk <jethrodaniel@gmail.com>
2020-04-14 21:56:40 -0500

    doc configuration

--- lib/msh/configuration.rb
+++ lib/msh/configuration.rb
@@ -1,30 +1,56 @@
 # frozen_string_literal: true
 
 module Msh
+  # Configure Msh.
+  #
+  # The user's config file is the first of
+  #
+  # - `~/.mshrc`
+  # - `$XDG_CONFIG_HOME/msh/config.rb`
+  # - `~/.config/msh/config.rb`
+  #
+  # It is Ruby, and is executed in the current shell, like string interpolation.
+  #
+  # A typical example might be
+  #
+  # ```
+  # Msh.configure do |c|
+  #   c.repl = :pry
+  # end
+  #
+  # def prompt
+  #   "$ "
+  # end
+  # ```
   class Configuration
-    attr_accessor :color, :history, :repl
+    # @return [bool] whether color is enabled (default: true)
+    attr_accessor :color
+
+    # @return [Integer] lines of history to keep (default: 2,048)
+    attr_accessor :history_lines
+
+    # @return [Symbol] what kind of repl to use, IRB or Pry (default: :irb)
+    attr_accessor :repl
 
     def initialize
       @color = true
-      @history = {:size => 2_048}
+      @history_lines = 2_048
       @repl = :irb # :pry
     end
   end
 
-  # Configure Msh like RSpec
+  # Configure Msh with a block
   #
   # ```
   # Msh.configure do |c|
-  #   c.color = true
-  #   c.history = {:size => 10.megabytes}
+  #   c.repl = :pry
   # end
   # ```
-  #
   def self.configure
     yield configuration if block_given?
   end
 
-  # Access Msh's configuration, like RSpec
+  # Access Msh's configuration
   def self.configuration
     @configuration ||= Msh::Configuration.new
   end
--- spec/msh/configuration_spec.rb
+++ spec/msh/configuration_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe Msh::Configuration do
     expect(Msh.configuration).not_to be nil
 
     expect(Msh.configuration.color).to be true
-    expect(Msh.configuration.history).to eq(:size => 2_048)
+    expect(Msh.configuration.history_lines).to eq(2_048)
     expect(Msh.configuration.repl).to eq(:irb)
 
     Msh.configure do |c|
       c.color = false
-      c.history = {:size => 1_000}
+      c.history_lines = 1_000
       c.repl = :pry
     end
 
     expect(Msh.configuration.color).to be false
-    expect(Msh.configuration.history).to eq(:size => 1_000)
+    expect(Msh.configuration.history_lines).to eq(1_000)
     expect(Msh.configuration.repl).to eq(:pry)
   end
 end
