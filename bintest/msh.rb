7f615139c8aef3ec83fa5adc0a7a229f859f0318
Mark Delk <jethrodaniel@gmail.com>
2020-05-27 02:10:09 -0500

    fix CI

--- bintest/msh.rb
+++ bintest/msh.rb
@@ -2,6 +2,20 @@
 
 require "msh"
 
+def it_lexes code, tokens
+  expected = Msh::Lexer.new(code).tokens.map(&:to_s)
+  assert_equal tokens, expected
+end
+
 assert "Msh" do
-  assert_equal "0.1.0", Msh::VERSION
+  assert_equal "0.2.0", Msh::VERSION
+end
+
+assert "Msh::Lexer" do
+  it_lexes "echo hi", [
+    '[1:1-4][WORD, "echo"]',
+    '[1:5-5][SPACE, " "]',
+    '[1:6-7][WORD, "hi"]',
+    '[1:8-8][EOF, "\\u0000"]'
+  ]
 end
