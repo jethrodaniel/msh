8223684ed0cfcf42d0c4bad67d33ebc02a941ad1
Mark Delk <jethrodaniel@gmail.com>
2020-07-01 22:49:37 -0500

    add example to Object#delegate

--- lib/msh/core_extensions.rb
+++ lib/msh/core_extensions.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Object
+  # Stupid simple method delegation, only supporting no args.
+  #
+  #     A = Struct.new(:foo) do
+  #       delegate :hi, :foo
+  #     end
+  #
+  #     class Foo
+  #       def hi
+  #         __method__
+  #       end
+  #     end
+  #
+  #     A.new(Foo.new).hi #=> :hi
+  #
   def self.delegate meth, obj, via: nil
     define_method meth do
       send(obj).send(via || meth)
