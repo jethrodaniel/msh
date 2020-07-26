26faeb8ee517d50a2436e27f9d33a9d789391051
Mark Delk <jethrodaniel@gmail.com>
2020-07-25 22:55:02 -0500

    start basic tab completion

--- lib/msh/readline.rb
+++ lib/msh/readline.rb
@@ -1,14 +1,21 @@
-require "reline" unless RUBY_ENGINE == "mruby"
+unless RUBY_ENGINE == "mruby"
+  require "reline"
+
+  Reline.completion_proc = -> input do
+    Dir.glob("#{input}*").sort
+  end
+
+  Reline.completion_append_character = " "
+  Reline.completer_quote_characters = "\"'"
+end
 
 module Msh
   module Readline
     def self.readline prompt, keep_history = true
-      if Object.const_defined? :Reline
-        ::Reline.readline(prompt, keep_history)
-      else
-        print prompt
-        gets&.chomp
-      end
+      return ::Reline.readline(prompt, keep_history) if Object.const_defined? :Reline
+
+      print prompt
+      gets&.chomp
     end
 
     def self.add_to_history line
