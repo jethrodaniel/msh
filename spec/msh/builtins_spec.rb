b042a08b97e6cfa4ea71ec395c7034256fd9bf79
Mark Delk <jethrodaniel@gmail.com>
2021-01-29 18:08:31 -0600

    split out builtins spec

---
+++ spec/msh/builtins_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+def msh cmd
+  sh "msh -c '#{cmd}'"
+end
+
+describe "msh builtins" do
+  describe "alias" do
+    it "shows all aliases when ran without args" do
+      _(msh("alias")).must_equal <<~SH
+        alias ls ls -lrth --color
+      SH
+    end
+    it "errors if an alias name is present, but no args" do
+      _(msh("alias foo")).must_include <<~SH
+        missing expansion for alias `foo` (RuntimeError)
+      SH
+    end
+    it "creates an alias for a series of words" do
+      _(msh("alias foo bar; foo")).must_include <<~SH
+        No such file or directory - bar
+      SH
+    end
+  end
+end
