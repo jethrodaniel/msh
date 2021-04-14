90ffb2a663ab7bf1c0b1f33f5d0bf012ea9ca1b3
Mark Delk <jethrodaniel@gmail.com>
2021-04-14 01:13:16 -0500

    simpler manpage task

---
+++ lib/tasks/man.rb
@@ -0,0 +1,55 @@
+require_relative "task"
+
+require "asciidoctor"
+
+module Msh
+  module Tasks
+    class Man < Task
+      CLEAN << 'man/man1'
+
+      def setup!
+        FileUtils.mkdir_p "man/man1"
+
+        Dir.glob("man/*.adoc").each do |adoc|
+          create_manpage!(adoc)
+        end
+      end
+
+      private
+
+      def create_manpage! adoc
+        cmd = File.basename(adoc, File.extname(adoc))
+        Asciidoctor.convert(
+          manpage_for(adoc),
+          :to_dir  => "man/man1/",
+          :to_file => "#{cmd}.1",
+          :doctype => "manpage",
+          :backend => "manpage",
+        )
+        puts "-> man/man1/#{cmd}.1"
+        sh "gzip -vf man/man1/#{cmd}.1"
+      end
+
+      def manpage_for adoc
+        <<~MAN
+          = #{File.basename(adoc)}(1)
+          :doctype: manpage
+          :release-version: #{Msh::VERSION}
+          :man manual: Msh Manual
+          :man source: Msh v#{Msh::VERSION}
+          :page-layout: base
+
+          #{File.read(adoc)}
+
+          == msh
+
+          Part of msh(1).
+        MAN
+      end
+    end
+  end
+end
+
+Msh::Tasks::Man.new \
+  "man",
+  "Generate manpages"
