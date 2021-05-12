a9cc273f4f6ea6dd0544198cae54c6028366a2b7
Mark Delk <jethrodaniel@gmail.com>
2021-05-11 23:38:25 -0500

    rm some comments

--- lib/msh/backports.rb
+++ lib/msh/backports.rb
@@ -11,38 +11,3 @@ ENV.instance_eval do
     merge! hsh
   end
 end unless ENV.respond_to?(:merge!) && ENV.respond_to?(:update)
-
-
-# version = RUBY_VERSION[0..2].to_f
-
-# if version <= 2.6
-#   class Binding
-#     def source_location
-#       self.eval "[__FILE__, __LINE__]"
-#     end
-#   end
-# end
-
-# if version <= 2.5
-#   class String
-#     def delete_suffix suffix
-#       self[0..size - suffix.size - 1]
-#     end
-#   end
-
-#   class Pathname
-#     def glob glob
-#       Dir.glob(glob)
-#     end
-#   end
-
-#   class Array
-#     alias prepend unshift
-#   end
-
-#   class Symbol
-#     def start_with? prefix
-#       to_s.start_with? prefix
-#     end
-#   end
-# end
