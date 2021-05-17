8e5890d728b59c6cb435bdd258e91aaa99c81e55
Mark Delk <jethrodaniel@gmail.com>
2021-05-16 23:22:57 -0500

    cleanup manpage a bit, cleanup config

--- lib/msh/config.rb
+++ lib/msh/config.rb
@@ -1,16 +1,18 @@
 module Msh
   module Config
     def self.config_text
-      paths = []
-
-      paths << File.join(ENV["XDG_CONFIG_HOME"], "msh/config.msh") if ENV.key? "XDG_CONFIG_HOME"
-
-      paths += [
+      paths = [
         File.join(Dir.home, ".mshrc"),
         File.join(Dir.home, ".config/msh/config.msh")
       ]
 
-      paths.select { |p| File.exist?(p) }.map { |f| File.read(f) }.join("\n")
+      if ENV.key?("XDG_CONFIG_HOME")
+        paths << File.join(ENV["XDG_CONFIG_HOME"], "msh/config.msh")
+      end
+
+      paths.select { |p| File.exist?(p) }
+           .map { |f| File.read(f) }
+           .join("\n")
     end
   end
 end
